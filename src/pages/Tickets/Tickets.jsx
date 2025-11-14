864176f3e4b3c542ab884ad103ddcf5e44c53d82
Aprendiz sistemas <Aprendizsistemas@cfc.local>
2025-11-14 10:36:35 -0500

    Correcciones finales para produccion

--- src/pages/Tickets/Tickets.jsx
+++ src/pages/Tickets/Tickets.jsx
@@ -207,6 +207,7 @@ const Tickets = () => {
   const stats = calculateStats();
 
   const handleCreate = () => {
+    console.log('handleCreate called');
     setFormData({
       title: '',
       description: '',
